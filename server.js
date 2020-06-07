db509e4551cba9b0bcc1163a07acab67f3f20357
Vinicius Santana <santanaviny@gmail.com>
2020-06-06 19:21:27 -0700

    Adding get all todos route

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const db = require('./models');
+const apiRoutes = require('./routes/apiRoutes');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use('/api', apiRoutes);
 
 db.sequelize.sync().then(() => {
     app.listen(PORT, () => {
         console.log(`listening on http://localhost:${PORT}`)
     })
-})
+});
