9802796d09cb32073aea221fc355491822329780
Vinicius Santana <santanaviny@gmail.com>
2020-06-07 11:30:18 -0700

    clean up

--- controllers/todo.js
+++ controllers/todo.js
@@ -1,31 +1,26 @@
 const db = require('../models');
 
 exports.getAllTodos = (req, res) => {
-    db.Todo.findAll().then(todos => res.send(todos));
+    db.Todo.findAll()
+        .then(todos => res.send(todos));
 }
 
 exports.getTodoById = (req, res) => {
-    db.Todo.findAll({
-        where: {
-            id: req.params.id
-        }
-    }).then(todo => res.send(todo));
+    db.Todo.findAll({ where: {id: req.params.id} })
+        .then(todo => res.send(todo));
 }
 
 exports.createTodo = (req, res) => {
-    db.Todo.create({
-        text: req.body.text
-    }).then(submitedTodo => res.send(submitedTodo));
+    db.Todo.create({ text: req.body.text })
+        .then(submitedTodo => res.send(submitedTodo));
 }
 
 exports.deleteTodo = (req, res) => {
-    db.Todo.destroy({
-        where: {
-            id: req.params.id
-        }
-    }).then(() => res.send("success"));
+    db.Todo.destroy({where: {id: req.params.id}})
+        .then(() => res.send("success"));
 }
 
 exports.editTodo = (req, res) => {
-    db.Todo.update({text: req.body.text},{where:{id: req.body.id}}).then(() => res.send("success"));
+    db.Todo.update({text: req.body.text},{where:{id: req.body.id}})
+        .then(() => res.send("success"));
 }
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const db = require('./models');
-const apiRoutes = require('./routes/todo');
+const todoRoutes = require('./routes/todo');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use('/api', apiRoutes);
+app.use('/api', todoRoutes);
 
 db.sequelize.sync().then(() => {
     app.listen(PORT, () => {
